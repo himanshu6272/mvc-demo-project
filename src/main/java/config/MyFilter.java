15be02835a6ca294800a3687d2a135913e24b609
himanshu-inexture <himanshup.inexture@gmail.com>
2023-07-20 18:17:08 +0530

    done changes in myFilter

--- src/main/java/config/MyFilter.java
+++ src/main/java/config/MyFilter.java
@@ -1,6 +1,7 @@
 package config;
 
 import model.User;
+import org.apache.log4j.Logger;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -10,7 +11,7 @@ import java.util.List;
 
 public class MyFilter implements Filter {
 
-
+    public static final Logger logger = Logger.getLogger(MyFilter.class);
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpSession session = req.getSession();
@@ -21,6 +22,7 @@ public class MyFilter implements Filter {
             flag = users.stream().anyMatch(user -> user.getEmail().equals(newUserEmail));
         }
         if(flag){
+            logger.info("User Already exists with this email!!!!!");
             response.setContentType("text/html");
             PrintWriter out = response.getWriter();
             out.println("<h1>User Already exists with this email!!!!!</h1>");
